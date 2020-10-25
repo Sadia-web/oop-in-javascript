9eb5d9aa8331b4777aabf05e00aeec3f57abc3bf
Sadia-web <sadiamim354@gmail.com>
2020-10-25 14:21:49 +0600

    Object Oriented Programming in Javascript

---
+++ encapsulation.js
@@ -0,0 +1,18 @@
+class Tiger{
+    #speed = 100;
+    location = 'Sundarban';
+    constructor(name){
+        this.name = name;
+        this.#speed = 125;
+    }
+    run(){
+        console.log(`I am ${this.name}. I am running at ${this.#speed}mph. I live in ${this.location}`)
+    }
+}
+const royal = new Tiger('Royal Bengal');
+royal.name = 'Pormanobic Bengal';
+royal.location = 'Borishal';
+
+royal.run();
+
+//Private variable or value, which can be used , but can naot access from outside
